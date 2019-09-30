4af75945e4d67d5ef7ace0fe10e60b5a010be00a
Mozafari1 <m_rahmat@hotmail.no>
2019-09-30 21:20:33 +0200

    updated

--- assigment_01/gcd.py
+++ assigment_01/gcd.py
@@ -5,7 +5,8 @@ b = int(input("Enter a value for B: "))
 def gcd(x,y):
     while (y):
         x, y = y, x%y
+        #print([y])
     return  x
 
-print ("The gcd for A =",a, "and B =",b, end=" => ")
+print ("The gcd for A =",a, "and B =",b, end=" => \n")
 print(gcd(a,b))
