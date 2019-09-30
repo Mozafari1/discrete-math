92e278b3156dd9151f52ffa3968a09d33e861c2f
Mozafari1 <m_rahmat@hotmail.no>
2019-09-30 20:40:10 +0200

    updated

--- assigment_01/Zm.py
+++ assigment_01/Zm.py
@@ -2,6 +2,8 @@
 a = int(input("Enter a value of A: \n"))
 b = int(input("Enter a value for B: \n"))
 m = int(input("Enter a value for m: \n"))
+
+# Addion
 def ZmAddion(x, y, m):
     SUM = x + y
     SUM = SUM% m
@@ -10,6 +12,7 @@ def ZmAddion(x, y, m):
 print("Det vilkorlige m er:",a+b,"mod",m,  end=" = ")
 print(ZmAddion(a,b,m))
 
+# Multiplication
 def ZmMultiplication (x, y, m):
     SUM = x*y
     SUM = SUM%m
@@ -17,7 +20,7 @@ def ZmMultiplication (x, y, m):
 
 print("Det vilkorlige m er:",a*b,"mod",m,  end=" = ")
 print(ZmMultiplication(a,b,m))
-
+# Subtraction
 def ZmSubtraction (x, y, m):
     SUM = x-y
     SUM = SUM%m
@@ -26,6 +29,7 @@ def ZmSubtraction (x, y, m):
 print("Det vilkorlige m er:",a-b,"mod",m,  end=" = ")
 print(ZmSubtraction(a,b,m))
 
+#   Division
 
 def ZmDivision (x, y, m):
     SUM = x/y
