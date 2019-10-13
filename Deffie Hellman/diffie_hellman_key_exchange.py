58924ac4169a49e5c083b36517f3725b475d1ab6
Mozafari1 <m_rahmat@hotmail.no>
2019-10-13 23:55:45 +0200

    updated diffie hellman

--- Deffie Hellman/diffie_hellman_key_exchange.py
+++ Deffie Hellman/diffie_hellman_key_exchange.py
@@ -12,6 +12,10 @@ def private_key (prime_number, prim_root):
     print("key_Bod   : %s"%(pow(key_Alice, Bob_rand_num, prime_number)))
 
 if __name__ == "__main__":
-    prime_number =774647675834763765353857776346583475865476867346576573456878436587436873465874395256984657345436578347658743658345
-    prim_root = 34524347586347567864358734856843758347568347657346575345763765847958436587456835874567349587583465887578743654
+    pn = random.randint(2934845,35767465)
+    pr = random.randint(2643766,568789678)
+    pn_pow = pow(pr, pn)
+    pr_pow = pow(pn,pr)
+    prime_number = pn_pow
+    prim_root = pr_pow
     private_key( prime_number, prim_root)
